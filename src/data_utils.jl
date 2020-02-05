2252f8dbd8bf57236acea92f3bf8809a908357c5
Thibaut Lienart <tlienart@me.com>
2020-02-05 23:19:30 +0100

    closes #7 (#8)

--- src/data_utils.jl
+++ src/data_utils.jl
@@ -241,10 +241,12 @@ selectcols(::LightInterface, ::Val{:table}, X, c; kw...) =
 """
     select(X, r, c)
 
-Select element(s) of a table or matrix at row(s) `r` and column(s) `c`. In the
-case of sparse data where the key `(r, c)`, zero or `missing` is returned,
-depending on the value type. See also: [`selectrows`](@ref),
-[`selectcols`](@ref).
+Select element(s) of a table or matrix at row(s) r and column(s) c. An object
+of the sink type of X (or a matrix) is returned unless c is a single integer or
+symbol. In that case a vector is returned, unless r is a single integer, in
+which case a single element is returned.
+
+See also: [`selectrows`](@ref), [`selectcols`](@ref).
 """
 select(X, r, c) = select(get_interface_mode(), vtrait(X), X, r, c)
 

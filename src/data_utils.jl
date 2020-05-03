4999f95ca9d7a4d8a1fdfca24a6dfb8c8d282d46
Shuhei Kadowaki <40514306+aviatesk@users.noreply.github.com>
2020-05-03 19:17:31 +0900

    `schema` interface (#39)
    * `schema` interface
    
    * throw any input with LightInterface anyway
    
    * add tests

--- src/data_utils.jl
+++ src/data_utils.jl
@@ -124,6 +124,23 @@ classes(x) = classes(get_interface_mode(), x)
 classes(::LightInterface, x) = errlight("classes")
 
 # ------------------------------------------------------------------------
+# schema
+
+"""
+    schema(X)
+
+If `X` is tabular, inspect the column types and scitypes, otherwise return
+`nothing`.
+"""
+schema(X; kw...) = schema(get_interface_mode(), vtrait(X), X; kw...)
+
+schema(::Mode, ::Val{:other}, X; kw...) = nothing
+
+schema(::LightInterface, ::Val{:other}, X; kw...) = errlight("schema")
+
+schema(::LightInterface, ::Val{:table}, X; kw...) = errlight("schema")
+
+# ------------------------------------------------------------------------
 # decoder
 
 """

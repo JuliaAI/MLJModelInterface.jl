532aaa349afb9acd8fcdaf04651cd11005277f18
Anthony D. Blaom <anthony.blaom@gmail.com>
2021-05-14 11:21:09 +1200

    "generalize" definition of `is_same_except` and `==` for MLJType

--- src/equality.jl
+++ src/equality.jl
@@ -1,45 +1,116 @@
+function _equal_to_depth_one(x1, x2)
+    names = propertynames(x1)
+    names === propertynames(x2) || return false
+    for name in names
+            getproperty(x1, name) == getproperty(x2, name) || return false
+    end
+    return true
+end
+
+@doc """
+    deep_properties(::Type{<:MLJType})
+
+Given an `MLJType` subtype `M`, the value of this trait should be a
+tuple of any properties of `M` to be regarded as "deep".
+
+When two instances of type `M` are to be tested for equality, in the
+sense of `==` or `is_same_except`, then the values of a "deep"
+property (whose values are assumed to be of composite type) are deemed
+to agree if all corresponding properties *of those property values*
+are `==`.
+
+Any property of `M` whose values are themselves of `MLJType` are
+"deep" automatically, and should not be included in the trait return
+value.
+
+See also [`is_same_except`](@ref)
+
+### Example
+
+Consider an `MLJType` subtype `Foo`, with a single field of
+type `Bar` which is *not* a subtype of `MLJType`:
+
+    mutable struct Bar
+        x::Int
+    end
+
+    mutable struct Foo <: MLJType
+        bar::Bar
+    end
+
+Then the mutability of `Foo` implies `Foo(1) != Foo(1)` and so, by the
+definition `==` for `MLJType` objects (see [`is_same_except`](@ref))
+we have
+
+    Bar(Foo(1)) != Bar(Foo(1))
+
+However after the declaration
+
+    MLJModelInterface.deep_properties(::Type{<:Foo}) = (:bar,)
+
+We have
+
+    Bar(Foo(1)) == Bar(Foo(1))
+
 """
-    is_same_except(m1::MLJType, m2::MLJType, exceptions::Symbol...)
+StatisticalTraits.deep_properties
 
-Returns `true` only the following conditions all hold:
 
-- `m1` and `m2` have the same type.
+"""
+    is_same_except(m1, m2, exceptions::Symbol...; deep_properties=Symbol[])
 
-- `m1` and `m2` have the same undefined fields.
+If both `m1` and `m2` are of `MLJType`, return `true` if the
+following conditions all hold, and `false` otherwise:
 
-- Corresponding fields agree, or are listed as `exceptions`, or have
-  `AbstractRNG` as values (one or both)
+- `typeof(m1) === typeof(m2)`
 
-Here "agree" is in the sense of "==", unless the objects are themselves of
-`MLJType`, in which case agreement is in the sense of `is_same_except` with
-no exceptions allowed.
+- `propertynames(m1) === propertynames(m2)`
 
-Note that Base.== is overloaded such that `m1 == m2` if and only if
-`is_same_except(m1, m2)`.
+- with the exception of properties listed as `exceptions` or bound to
+  an `AbstractRNG`, each pair of corresponding property values is
+  either "equal" or  both undefined.
+
+The meaining of "equal" depends on the type of the property value:
+
+- values that are themselves of `MLJType` are "equal" if they are
+equal in the sense of `is_same_except` with no exceptions.
+
+- values that are not of `MLJType` are "equal" if they are `==`.
+
+In the special case of a "deep" property, "equal" has a different
+meaning; see [`deep_properties`](@ref)) for details.
+
+If `m1` or `m2` are not `MLJType` objects, then return `==(m1, m2)`.
+
 """
 is_same_except(x1, x2) = ==(x1, x2)
-function is_same_except(m1::M1, m2::M2,
-            exceptions::Symbol...) where {M1<:MLJType,M2<:MLJType}
-    if typeof(m1) != typeof(m2)
-        return false
+function is_same_except(m1::M1,
+                        m2::M2,
+                        exceptions::Symbol...) where {M1<:MLJType,M2<:MLJType}
+    typeof(m1) === typeof(m2) || return false
+    names = propertynames(m1)
+    propertynames(m2) === names || return false
+
+    for name in names
+        if !(name in exceptions)
+            if !isdefined(m1, name)
+               !isdefined(m2, name) || return false
+            elseif isdefined(m2, name)
+                if name in deep_properties(M1)
+                    _equal_to_depth_one(getproperty(m1,name),
+                                        getproperty(m2, name)) || return false
+                else
+                    (is_same_except(getproperty(m1, name),
+                                    getproperty(m2, name)) ||
+                     getproperty(m1, name) isa AbstractRNG ||
+                     getproperty(m2, name) isa AbstractRNG) || return false
+                end
+            else
+                return false
+            end
+        end
     end
-    defined1 = filter(fieldnames(M1)|>collect) do fld
-        isdefined(m1, fld) && !(fld in exceptions)
-    end
-    defined2 = filter(fieldnames(M1)|>collect) do fld
-        isdefined(m2, fld) && !(fld in exceptions)
-    end
-    if defined1 != defined2
-        return false
-    end
-    same_values = true
-    for fld in defined1
-        same_values = same_values &&
-            (is_same_except(getfield(m1, fld), getfield(m2, fld)) ||
-             getfield(m1, fld) isa AbstractRNG ||
-             getfield(m2, fld) isa AbstractRNG)
-    end
-    return same_values
+    return true
 end
 
 ==(m1::M1, m2::M2) where {M1<:MLJType,M2<:MLJType} = is_same_except(m1, m2)

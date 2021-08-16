8a45a8edddcba2f8ef3c176f60c411f3e7788548
Anthony D. Blaom <anthony.blaom@gmail.com>
2021-08-17 08:40:04 +1200

    make fit_data_scitype(::Type{<:Model}) fallback `Unknown`

--- src/model_traits.jl
+++ src/model_traits.jl
@@ -21,7 +21,8 @@ for M in ABSTRACT_MODEL_SUBTYPES
     @eval(StatisticalTraits.abstract_type(::Type{<:$M}) = $M)
 end
 
-StatisticalTraits.fit_data_scitype(M::Type{<:Model}) = Tuple{input_scitype(M)}
+StatisticalTraits.fit_data_scitype(M::Type{<:Unsupervised}) =
+    Tuple{input_scitype(M)}
 StatisticalTraits.fit_data_scitype(::Type{<:Static}) = Tuple{}
 function StatisticalTraits.fit_data_scitype(M::Type{<:Supervised})
     I = input_scitype(M)

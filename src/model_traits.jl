4fdc1ab73e38ed9687a15d98b925d534380de1fe
Anthony D. Blaom <anthony.blaom@gmail.com>
2021-08-16 14:19:04 +1200

    integrate new traits: predict_scitype, etc

--- src/model_traits.jl
+++ src/model_traits.jl
@@ -16,3 +16,73 @@ StatisticalTraits.prediction_type(::Type{<:Interval})      = :interval
 implemented_methods(M::Type) = implemented_methods(get_interface_mode(), M)
 implemented_methods(model) = implemented_methods(typeof(model))
 implemented_methods(::LightInterface, M) = errlight("implemented_methods")
+
+for M in ABSTRACT_MODEL_SUBTYPES
+    @eval(StatisticalTraits.abstract_type(::Type{<:$M}) = $M)
+end
+
+StatisticalTraits.training_scitype(M::Type{<:Model}) = input_scitype(M)
+StatisticalTraits.training_scitype(::Type{<:Static}) = Tuple{}
+function StatisticalTraits.training_scitype(M::Type{<:Supervised})
+    I = input_scitype(M)
+    T = target_scitype(M)
+    ret = Tuple{I,T}
+    if supports_weights(M)
+        W = AbstractVector{Union{Continuous,Count}} # weight scitype
+        return Union{ret,Tuple{I,T,W}}
+    elseif supports_class_weights(M)
+        W = AbstractDict{Finite,Union{Continuous,Count}}
+        return Union{ret,Tuple{I,T,W}}
+    end
+    return ret
+end
+
+StatisticalTraits.transform_scitype(M::Type{<:Unsupervised}) =
+    output_scitype(M)
+
+StatisticalTraits.inverse_transform_scitype(M::Type{<:Unsupervised}) =
+    input_scitype(M)
+
+StatisticalTraits.predict_scitype(M::Type{<:Deterministic}) = target_scitype(M)
+
+
+## FALLBACKS FOR `predict_scitype` FOR `Probabilistic` MODELS
+
+# This seems less than ideal but should reduce the number of `Unknown`
+# in `prediction_type` for models which, historically, have not
+# implemented the trait.
+
+StatisticalTraits.predict_scitype(M::Type{<:Probabilistic}) =
+    _density(target_scitype(M))
+
+_density(::Any) = Unknown
+for T in [:Continuous, :Count, :Textual]
+    eval(quote
+         _density(::Type{AbstractArray{$T,D}}) where D =
+         AbstractArray{Density{$T},D}
+         end)
+end
+
+for T in [:Finite,
+          :Multiclass,
+          :OrderedFactor,
+          :Infinite,
+          :Continuous,
+          :Count,
+          :Textual]
+    eval(quote
+         _density(::Type{AbstractArray{<:$T,D}}) where D =
+         AbstractArray{Density{<:$T},D}
+         _density(::Type{Table($T)}) = Table(Density{$T})
+         end)
+end
+
+for T in [:Finite, :Multiclass, :OrderedFactor]
+    eval(quote
+         _density(::Type{AbstractArray{<:$T{N},D}}) where {N,D} =
+         AbstractArray{Density{<:$T{N}},D}
+         _density(::Type{AbstractArray{$T{N},D}}) where {N,D} =
+         AbstractArray{Density{$T{N}},D}
+         _density(::Type{Table($T{N})}) where N = Table(Density{$T{N}})
+         end)
+end
